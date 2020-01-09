2ab742676e5842452a996e486449c45dbaa675d5
Tomas Gomez <tgomezm@ripley.com>
2020-01-09 01:31:09 -0300

    modal medidas

--- pages/MedidasStack/Home.js
+++ pages/MedidasStack/Home.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import {
-  View, Text, StyleSheet, ActivityIndicator,
+  View, Text, StyleSheet, ActivityIndicator,Modal,Alert, TextInput, TouchableOpacity
 } from 'react-native';
 import Button from '../../components/Utils/Button';
 import { Email } from '../../models/API';
+import { Ionicons } from '@expo/vector-icons';
+import { Formik } from 'formik';
+
+function FilaModal(props) {
+  const {tipo, medida} = props;
+  let { bbw } = props;
+  bbw = typeof bbw === 'undefined' ? 2 : 0;
+
+  return (
+    <View style={{
+      flexDirection: 'row', marginVertical: 8, paddingBottom: 5,
+    }}
+    >
+      <Text style={{ flex: 1, color: 'grey' }}>{tipo}</Text>
+      <TextInput placeholder={`       `} value={values.medida} keyboardType='numeric' maxLength={3} 
+      onEndEditing={handleChange(medida)} style={{ textAlign: 'right', color: 'grey', borderBottomWidth: bbw,borderColor: '#ddd' }}>{}</TextInput>
+    </View>
+    )
+}
 
 function FilaMedida(props) {
   const { tipo, medida } = props;
@@ -39,14 +58,25 @@ export default class MedidasHome extends Component {
       medidas: null,
       sexo: null,
       done: false,
-    };
+      modalVisible: false,
+  }
+}
+
+  openModal() {
+    this.setState({modalVisible:true});
+  }
+
+  closeModal(props) {
+    const { navigation } = this.props;
+
+    navigation.push('Medidas')
+    this.setState({modalVisible:false});
   }
 
   componentDidMount() {
     this.getMedidas(); // Obtenemos las medidas en el Home
   }
-
-  async getMedidas() {
+    async getMedidas() {
     console.log('Home::getMedidas');
 
     const u = new Email();
@@ -71,9 +101,11 @@ export default class MedidasHome extends Component {
       bTieneMedidas,
       medidas,
       bEsFemenino,
-      done: true
+      done: true,
     });
+    
   }
+
 
   render() {
     const { done } = this.state;
@@ -94,14 +126,18 @@ export default class MedidasHome extends Component {
 
         filaP = <FilaMedida tipo="Pecho" medida={medidas.chest} bbw="0" />
         filaB = null
+        filaP_Modal = <FilaModal tipo="Pecho" medida={medidas.chest} bbw="0" />
+        filaB_Modal = null
 
         if (bEsFemenino) {
           filaP = <FilaMedida tipo="Pecho" medida={medidas.chest} />
           filaB = <FilaMedida tipo="Busto" medida={medidas.bust} bbw="0" />
+          filaP_Modal = <FilaModal tipo="Pecho" medida={medidas.chest} />
+          filaB_Modal = <FilaModal tipo="Busto" medida={medidas.bust} bbw="0" />
         }
-
         return (
           <View style={styles.container}>
+            <View style={styles.container}>
             {encabezado}
             <View style={styles.marco}>
               <FilaMedida tipo="Brazo Izquierdo" medida={medidas.left_arm} />
@@ -117,14 +153,69 @@ export default class MedidasHome extends Component {
             </View>
             <View>
               <Button text="Actualizar medidas" onPress={() => navigation.navigate('Altura')} />
-              <Button text="RESULTADOS" onPress={() => {
-                navigation.push('Resultados', {
-                  height: 1,
-                  frontal: null,
-                  lateral: null,
-                });
-              }} />
+              <Button
+                  onPress={() => this.openModal()}
+                  title="Open modal"
+                  text="Editar medidas manualmente"
+              />
+              
             </View>
+            <View style={styles.container}>
+            <Modal
+              visible={this.state.modalVisible}
+              animationType={'slide'}
+              onRequestClose={() => this.closeModal()}
+          >
+            <TouchableOpacity 
+                                            onPress={() => {
+                                            this.closeModal()
+                                        }}>
+                                          <Ionicons name="md-close" size={20} color='red' />
+                                        
+                                        </TouchableOpacity>
+            <View style={styles.containerModal}>
+                <Text style={{color:'grey', fontSize: 20, justifyContent: 'center'}}>Ingresar todas las medidas correspondientes</Text>
+              <View style={styles.marcoModal}>
+              <Formik
+                  initialValues={{ 
+                    BrazoIzquierdo: '' ,
+                    BrazoDerecho: '',
+                    PiernaIzquierda: '',
+                    PiernaDerecha: '',
+                    Cintura: '',
+                    Cadera: '',
+                }}
+                  onSubmit={values => console.log(values)}
+                >
+                  {({ handleChange, handleBlur, handleSubmit, values }) => (
+                    <View>
+                      <FilaModal tipo="Brazo Izquierdo" medida={medidas.left_arm} />
+                    <FilaModal tipo="Brazo Derecho" medida={medidas.right_arm} />
+      
+                    <FilaModal tipo="Pierna Izquierda" medida={medidas.left_leg} />
+                    <FilaModal tipo="Pierna Derecha" medida={medidas.right_leg} />
+      
+                    <FilaModal tipo="Cintura" medida={medidas.waist} />
+                    <FilaModal tipo="Cadera" medida={medidas.hips} />
+                      <Button onPress={handleSubmit} title="Submit" />
+                    </View>
+                  )}
+              {filaP_Modal}
+              {filaB_Modal}
+                </Formik>
+                <Button
+                    onPress={() => {
+                      this.closeModal()}}
+                    title="Guardar medidas"
+                    text="Guardar medidas"
+                >
+                </Button>
+              </View>
+            </View>
+          </Modal>
+          </View>
+
+          </View>
           </View>
         );
       }
@@ -135,7 +226,7 @@ export default class MedidasHome extends Component {
             <Text style={{ color: '#8E8E8E' }}>¡Aún no has calculado tus medidas!</Text>
             <Button text="Obtén tus medidas" onPress={() => navigation.navigate('Altura')} />
             <Text style={{ color: '#8E8E8E' }}>Si lo prefieres, puedes ingresar tus medidas manualmente</Text>
-            <Button text="Ingresar manualmente" onPress={() => navigation.navigate('Altura')} />
+            <Button text="Ingresar manualmente" onPress={() => this.openModal()} />
           </View>
         )
       }
@@ -162,6 +253,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerModal: {
+    paddingTop: "30%",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  exitModal: {
+    paddingTop: "500%",
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingRight: "17%",
+    paddingLeft: "20%",
+    justifyContent: 'center',
+  },
   marco: {
     padding: 20,
     width: '70%',
@@ -176,6 +280,22 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.23,
     shadowRadius: 1,
     elevation: 2,
+  },
+  marcoModal: {
+    marginTop: '10%',
+    padding: 20,
+    width: '70%',
+    borderWidth: 0,
+    borderRadius: 10,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 1,
+    elevation: 1,
   },
   titulo: {
     fontSize: 16,
